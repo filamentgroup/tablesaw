e2e6f90d092be040c6598d5b5524985ff93cb5ca
Zach Leatherman <zachleatherman@gmail.com>
2013-12-19 17:01:34 -0600

    Adds respond for IE8 support

--- src/tablesaw.js
+++ src/tablesaw.js
@@ -9,7 +9,15 @@
 	if( !( 'querySelector' in document ) || ( window.blackberry && window.WebKitPoint ) || window.operamini ) {
 		return;
 	} else {
-		$( document.documentElement ).addClass( 'tablesaw-enhanced' );
+		var div = document.createElement('div'),
+			all = div.getElementsByTagName('i'),
+			$doc = $( document.documentElement );
+
+		div.innerHTML = '<!--[if lte IE 8]><i></i><![endif]-->';
+		if( all[ 0 ] ) {
+			$doc.addClass( 'ie-lte8' );
+		}
+		$doc.addClass( 'tablesaw-enhanced' );
 
 		// DOM-ready auto-init of plugins.
 		// Many plugins bind to an "enhance" event to init themselves on dom ready, or when new markup is inserted into the DOM
