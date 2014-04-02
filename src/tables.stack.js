c2a2ceda0f685a6b70ce116770abc0f09ddb64c0
Zach Leatherman <zleatherman@ebay.com>
2014-04-02 10:51:35 -0500

    Adds data-no-labels to opt out of the JS inline label creation. Tables will still reflow (but will basically be CSS only).

--- src/tables.stack.js
+++ src/tables.stack.js
@@ -7,38 +7,50 @@
 
 ;(function( win, $, undefined ){
 
+	var classes = {
+		stackTable: 'tablesaw-stack',
+		cellLabels: 'tablesaw-cell-label'
+	};
+
+	var data = {
+		obj: 'tablesaw-stack'
+	};
+
+	var attrs = {
+		labelless: 'data-no-labels'
+	};
+
 	var Stack = function( element ) {
 
 		this.$table = $( element );
 
-		this.classes = {
-			stackTable: "tablesaw-stack",
-			cellLabels: "tablesaw-cell-label"
-		};
+		this.labelless = this.$table.is( '[' + attrs.labelless + ']' );
 
-		// allHeaders references headers, plus all THs in the thead, which may include several rows, or not
-		this.allHeaders = this.$table.find( "th" );
+		if( !this.labelless ) {
+			// allHeaders references headers, plus all THs in the thead, which may include several rows, or not
+			this.allHeaders = this.$table.find( "th" );
+		}
 
-		this.$table.data( 'tablesaw-stack', this );
+		this.$table.data( data.obj, this );
 	};
 
 	Stack.prototype.init = function( colstart ) {
-		this.$table.addClass( this.classes.stackTable );
+		this.$table.addClass( classes.stackTable );
+
+		if( this.labelless ) {
+			return;
+		}
 
 		// get headers in reverse order so that top-level headers are appended last
-
 		var reverseHeaders = $( this.allHeaders );
 
 		// create the hide/show toggles
-		var self = this;
-
 		reverseHeaders.each(function(){
 			var $cells = $( this.cells ),
 				hierarchyClass = $cells.not( this ).filter( "thead th" ).length && " tablesaw-cell-label-top",
 				text = $(this).text();
 
-			if( text !== ""  ){
-
+			if( text !== "" ){
 				if( hierarchyClass ){
 					var iteration = parseInt( $( this ).attr( "colspan" ), 10 ),
 						filter = "";
@@ -46,18 +58,17 @@
 					if( iteration ){
 						filter = "td:nth-child("+ iteration +"n + " + ( colstart ) +")";
 					}
-					$cells.filter( filter ).prepend( "<b class='" + self.classes.cellLabels + hierarchyClass + "'>" + text + "</b>"  );
-				}
-				else {
-					$cells.prepend( "<b class='" + self.classes.cellLabels + "'>" + text + "</b>"  );
+					$cells.filter( filter ).prepend( "<b class='" + classes.cellLabels + hierarchyClass + "'>" + text + "</b>"  );
+				} else {
+					$cells.prepend( "<b class='" + classes.cellLabels + "'>" + text + "</b>"  );
 				}
 			}
 		});
 	};
 
 	Stack.prototype.destroy = function() {
-		this.$table.removeClass( this.classes.stackTable );
-		this.$table.find( '.' + this.classes.cellLabels ).remove();
+		this.$table.removeClass( classes.stackTable );
+		this.$table.find( '.' + classes.cellLabels ).remove();
 	};
 
 	// on tablecreate, init
@@ -72,7 +83,7 @@
 	$( document ).on( "tablesawdestroy", "table", function( e, mode ){
 
 		if( mode === 'stack' ){
-			$( this ).data( 'tablesaw-stack' ).destroy();
+			$( this ).data( data.obj ).destroy();
 		}
 
 	} );
