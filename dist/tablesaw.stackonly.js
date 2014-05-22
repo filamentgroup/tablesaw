2cc9fecca93da6edaec19b28a1c91dca40349dc8
Zach Leatherman <zachleatherman@gmail.com>
2014-05-22 17:21:33 -0500

    New dist files.

--- dist/tablesaw.stackonly.js
+++ dist/tablesaw.stackonly.js
@@ -1,6 +1,30 @@
-/*! Tablesaw - v0.1.2 - 2014-05-19
+/*! Tablesaw - v0.1.3 - 2014-05-19
 * https://github.com/filamentgroup/tablesaw
 * Copyright (c) 2014 Filament Group; Licensed MIT */
+;(function( $ ) {
+	var div = document.createElement('div'),
+		all = div.getElementsByTagName('i'),
+		$doc = $( document.documentElement );
+
+	div.innerHTML = '<!--[if lte IE 8]><i></i><![endif]-->';
+	if( all[ 0 ] ) {
+		$doc.addClass( 'ie-lte8' );
+	}
+
+	// Cut the mustard
+	if( !( 'querySelector' in document ) || ( window.blackberry && !window.WebKitPoint ) || window.operamini ) {
+		return;
+	} else {
+		$doc.addClass( 'tablesaw-enhanced' );
+
+		// DOM-ready auto-init of plugins.
+		// Many plugins bind to an "enhance" event to init themselves on dom ready, or when new markup is inserted into the DOM
+		$( function(){
+			$( document ).trigger( "enhance.tablesaw" );
+		});
+	}
+
+})( jQuery );
 ;(function( $ ) {
 	var pluginName = "table",
 		classes = {
