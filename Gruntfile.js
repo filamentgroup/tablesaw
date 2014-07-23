f88570bb4a53965b4e65e25cc5411de6ea133c43
Zach Leatherman <zachleatherman@gmail.com>
2014-07-23 11:35:53 -0500

    Fixes #2 to clean up npm package description on npmjs.org

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		// Metadata.
-		pkg: grunt.file.readJSON('tablesaw.json'),
+		pkg: grunt.file.readJSON('package.json'),
 		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
 			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
