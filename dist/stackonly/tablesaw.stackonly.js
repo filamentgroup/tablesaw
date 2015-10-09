0cc0470df7a36d3a7708452642b8c753a7a77af4
Zach Leatherman <zachleatherman@gmail.com>
2015-10-09 17:18:21 -0500

    Tablesaw 2.0.1: Mountain Hemlock

--- dist/stackonly/tablesaw.stackonly.js
+++ dist/stackonly/tablesaw.stackonly.js
@@ -1,4 +1,4 @@
-/*! Tablesaw - v2.0.0 - 2015-10-09
+/*! Tablesaw - v2.0.1 - 2015-10-09
 * https://github.com/filamentgroup/tablesaw
 * Copyright (c) 2015 Filament Group; Licensed MIT */
 /*
@@ -91,7 +91,7 @@ if( Tablesaw.mustard ) {
 				}
 
 				// Store "cells" data on header as a reference to all cells in the same column as this TH
-				this.cells = self.$table.find("tr").not( thrs[0] ).not( this ).children( sel );
+				this.cells = self.$table.find("tr").not( thrs[0] ).not( this ).children().filter( sel );
 				coltally++;
 			});
 		});
