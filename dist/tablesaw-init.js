b8067da30adb9b5552c809db03805cbfdd0e1547
Zach Leatherman <zachleatherman@gmail.com>
2016-10-12 17:26:05 -0500

    Fixes #241

--- dist/tablesaw-init.js
+++ dist/tablesaw-init.js
@@ -1,9 +1,16 @@
-/*! Tablesaw - v3.0.0-beta.3 - 2016-10-10
+/*! Tablesaw - v3.0.0-beta.4 - 2016-10-12
 * https://github.com/filamentgroup/tablesaw
 * Copyright (c) 2016 Filament Group; Licensed MIT */
 ;(function( win ) {
 
-	var $ = 'jQuery' in win ? jQuery : shoestring;
+	var $;
+	if( 'shoestring' in win ) {
+		$ = win.shoestring;
+	} else if( 'jQuery' in win ) {
+		$ = win.jQuery;
+	} else {
+		throw new Error( "tablesaw: DOM library not found." );
+	}
 
 	// DOM-ready auto-init of plugins.
 	// Many plugins bind to an "enhance" event to init themselves on dom ready, or when new markup is inserted into the DOM
--- src/tables-init.js
+++ src/tables-init.js
@@ -6,7 +6,14 @@
 
 ;(function( win ) {
 
-	var $ = 'jQuery' in win ? jQuery : shoestring;
+	var $;
+	if( 'shoestring' in win ) {
+		$ = win.shoestring;
+	} else if( 'jQuery' in win ) {
+		$ = win.jQuery;
+	} else {
+		throw new Error( "tablesaw: DOM library not found." );
+	}
 
 	// DOM-ready auto-init of plugins.
 	// Many plugins bind to an "enhance" event to init themselves on dom ready, or when new markup is inserted into the DOM
