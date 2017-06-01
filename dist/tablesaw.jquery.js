485bfb391108c61ac85e838d9786f4d308c952b5
Zach Leatherman <zachleatherman@gmail.com>
2017-06-01 16:37:44 -0500

    v3.0.1

--- dist/tablesaw.jquery.js
+++ dist/tablesaw.jquery.js
@@ -1,4 +1,4 @@
-/*! Tablesaw - v3.0.1-beta.21 - 2017-04-25
+/*! Tablesaw - v3.0.1 - 2017-06-01
 * https://github.com/filamentgroup/tablesaw
 * Copyright (c) 2017 Filament Group; Licensed MIT */
 // UMD module definition
