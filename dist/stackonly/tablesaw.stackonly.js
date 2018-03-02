690e0022e821d0bff650dd4685b2d9e47bd9e9d3
Daniel <daniel.sabados@gmail.com>
2018-03-02 11:42:51 +1100

    adding labelless to the cell level

--- dist/stackonly/tablesaw.stackonly.js
+++ dist/stackonly/tablesaw.stackonly.js
@@ -3,7 +3,7 @@
 * Copyright (c) 2018 Filament Group; Licensed MIT */
 /*! Shoestring - v2.0.0 - 2017-02-14
 * http://github.com/filamentgroup/shoestring/
-* Copyright (c) 2017 Scott Jehl, Filament Group, Inc; Licensed MIT & GPLv2 */ 
+* Copyright (c) 2017 Scott Jehl, Filament Group, Inc; Licensed MIT & GPLv2 */
 (function( factory ) {
 	if( typeof define === 'function' && define.amd ) {
 			// AMD. Register as an anonymous module.
@@ -934,7 +934,7 @@
 	 * @this shoestring
 	 */
 	shoestring.fn.next = function(){
-		
+
 		var result = [];
 
 		// TODO need to implement map
@@ -1050,7 +1050,7 @@
 	 * @this shoestring
 	 */
 	shoestring.fn.prev = function(){
-		
+
 		var result = [];
 
 		// TODO need to implement map
@@ -1090,7 +1090,7 @@
 	 * @this shoestring
 	 */
 	shoestring.fn.prevAll = function(){
-		
+
 		var result = [];
 
 		this.each(function() {
@@ -1213,7 +1213,7 @@
 	 * @this shoestring
 	 */
 	shoestring.fn.siblings = function(){
-		
+
 		if( !this.length ) {
 			return shoestring( [] );
 		}
@@ -1271,7 +1271,7 @@
 	 * @this shoestring
 	 */
 	shoestring.fn.text = function() {
-		
+
 		return getText( this );
 	};
 
@@ -1529,7 +1529,7 @@
 
 	shoestring.fn.on = shoestring.fn.bind;
 
-	
+
 
 
 	/**
@@ -1542,7 +1542,7 @@
 	 */
 	shoestring.fn.unbind = function( event, callback ){
 
-		
+
 		var evts = event ? event.split( " " ) : [];
 
 		return this.each(function(){
@@ -2269,6 +2269,8 @@ if (Tablesaw.mustard) {
 			})
 			.filter(function() {
 				return (
+					!$(this)
+					  .is("[" + attrs.labelless + "]") &&
 					!$(this)
 						.closest("tr")
 						.is("[" + attrs.labelless + "]") &&
